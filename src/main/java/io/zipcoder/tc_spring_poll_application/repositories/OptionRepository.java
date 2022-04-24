945f0563dd89b3960fd09afa38226b9da51548c1
Ethan <ethan@zipcoders-MacBook-Pro-4.local>
2022-04-24 16:19:41 -0400

    getting Started

--- src/main/java/io/zipcoder/tc_spring_poll_application/repositories/OptionRepository.java
+++ src/main/java/io/zipcoder/tc_spring_poll_application/repositories/OptionRepository.java
@@ -1,6 +1,9 @@
 package io.zipcoder.tc_spring_poll_application.repositories;
 
+import io.zipcoder.tc_spring_poll_application.Option;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
-public interface OptionRepository extends CrudRepository<io.zipcoder.tc_spring_poll_application.Option,Long> {
+@Repository
+public interface OptionRepository extends CrudRepository<Option,Long> {
 }
--- src/main/java/io/zipcoder/tc_spring_poll_application/repositories/VoteRepository.java
+++ src/main/java/io/zipcoder/tc_spring_poll_application/repositories/VoteRepository.java
@@ -1,7 +1,12 @@
 package io.zipcoder.tc_spring_poll_application.repositories;
-
 import io.zipcoder.tc_spring_poll_application.Vote;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
-public interface VoteRepository extends CrudRepository<Vote,Long> {
+public interface VoteRepository extends CrudRepository<Vote, Long> {
+    @Query(value = "SELECT v.* " +
+            "FROM io.zipcoder.tc_spring_poll_application.Option o, io.zipcoder.tc_spring_poll_application.Vote v " +
+            "WHERE o.POLL_ID = ?1 " +
+            "AND v.OPTION_ID = o.OPTION_ID", nativeQuery = true)
+    public Iterable<Vote> findVotesByPoll(Long pollId);
 }
